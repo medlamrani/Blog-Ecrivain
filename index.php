3e8d85dda87e171bc302024b4db9a825e3a947cc
medlamrani <45313009+medlamrani@users.noreply.github.com>
2020-04-27 04:40:04 +0000

    new file

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <form action="secret.php" method="post">
+            <p>
+                <label> Etes vous celui que vous pretender etre ?<input type="radio" name="question"/></label></br>
+                <label><input type="password" name="mot_de_passe"/></label>
+                <label><input type="submit" value="valider"/></label>
+            
+            </p>
+        </form>
+    </p>
+</body>
+</html>
